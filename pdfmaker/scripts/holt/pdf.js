ec570c859a53307b80acecd33aff7ca5f6742392
matt.retzer <matthew.retzer@macmillan.com>
2016-10-04 15:29:04 -0400

    found string-set wouldn't work for non-printing objects-retrieving and writing Chapter/section names via the .js

--- pdfmaker/scripts/holt/pdf.js
+++ pdfmaker/scripts/holt/pdf.js
@@ -18,13 +18,13 @@ function moveIllustrationSource()
 var illoSources = document.getElementsByClassName("IllustrationSourceis");
 var copyright = getAllElementsWithAttribute('section',"data-type",'copyright-page')[0];
 for (var j = 0; illoSources.length > j; j++) {
-	var figID = illoSources[j].parentNode.getAttribute('id');
-	var figLink = illoSources[j].childNodes[0];
-	figLink.href = '#' + figID;
+  var figID = illoSources[j].parentNode.getAttribute('id');
+  var figLink = illoSources[j].childNodes[0];
+  figLink.href = '#' + figID;
   if (illoSources[j].parentNode.parentNode.getAttribute('class') != 'abouttheauthor') {
-	  copyright.appendChild(illoSources[j]);
+    copyright.appendChild(illoSources[j]);
   };
-	};
+  };
 };
 
 function addRunningElements() {
@@ -33,30 +33,46 @@ function addRunningElements() {
     var thisParent = allParas[q];
     var section = allParas[q].parentNode;
     var i = 0;
-    var testvar = 12;
+    var sectionHead = "NoChapterName";
     while (section.tagName != "SECTION" && section.tagName != "NAV" && i < 10) {
       var section = section.parentNode;
       i++;
     };
     var sectionType = section.getAttribute('data-type')
     if (sectionType == "preface" || sectionType == "toc") {
+    if (section.querySelector('h1:first-of-type') != null) {
+    var sectionHead = section.querySelector('h1:first-of-type').textContent;
+    };
+
       var frontRunHeadRight = document.createElement("div");
       var frontRunHeadRightSpan = document.createElement("span");
       frontRunHeadRight.setAttribute("class", "frontrunheadright");
       frontRunHeadRight.appendChild(frontRunHeadRightSpan);
+      frontRunHeadRightSpan.textContent=sectionHead;      
       thisParent.parentNode.insertBefore(frontRunHeadRight, thisParent.nextSibling);
 
       var frontRunHeadLeft = document.createElement("div");
       var frontRunHeadLeftSpan = document.createElement("span");
       frontRunHeadLeft.setAttribute("class", "frontrunheadleft");
       frontRunHeadLeft.appendChild(frontRunHeadLeftSpan);
+    frontRunHeadLeftSpan.textContent=sectionHead;
       thisParent.parentNode.insertBefore(frontRunHeadLeft, thisParent.nextSibling);
     };
     if (sectionType == "chapter") {
+    if (section.querySelector('h1.ChapTitlect') != null) {
+          var sectionHead = section.querySelector('h1.ChapTitlect').textContent;
+    } else if (section.querySelector('h1.ChapTitleNonprintingctnp') != null) {
+        var sectionHead = section.querySelector('h1.ChapTitleNonprintingctnp').textContent;
+    };
+    if (sectionHead.length < 4 && sectionHead.match(/^[0-9]+$/) != null) {
+      sectionHead = "Chapter " + sectionHead;
+    };
+
       var runHeadRight = document.createElement("div");
       var runHeadRightSpan = document.createElement("span");
       runHeadRight.setAttribute("class", "runheadright");
       runHeadRight.appendChild(runHeadRightSpan);
+      runHeadRightSpan.textContent=sectionHead;
       thisParent.parentNode.insertBefore(runHeadRight, thisParent.nextSibling);
 
       var bookTitle = BKMKRINSERTBKTITLE;
@@ -68,16 +84,22 @@ function addRunningElements() {
       thisParent.parentNode.insertBefore(runHeadLeft, thisParent.nextSibling);
     };
     if (sectionType == 'appendix') {
+    if (section.querySelector('h1[class*="BMHead"]') != null) {
+    var sectionHead = section.querySelector('h1[class*="BMHead"]').textContent;
+    };
+      
       var runHeadRight = document.createElement("div");
       var runHeadRightSpan = document.createElement("span");
       runHeadRight.setAttribute("class", "runheadright");
       runHeadRight.appendChild(runHeadRightSpan);
+      runHeadRightSpan.textContent=sectionHead;
       thisParent.parentNode.insertBefore(runHeadRight, thisParent.nextSibling);
 
       var backRunHeadLeft = document.createElement("div");
       var backRunHeadLeftSpan = document.createElement("span");
       backRunHeadLeft.setAttribute("class", "backrunheadleft");
       backRunHeadLeft.appendChild(backRunHeadLeftSpan);
+      backRunHeadLeftSpan.textContent=sectionHead;      
       thisParent.parentNode.insertBefore(backRunHeadLeft, thisParent.nextSibling);
     };
   };
@@ -144,9 +166,9 @@ function fullpageFigures() {
 };
 
 window.onload = function() {
-  moveIllustrationSource();
+  //moveIllustrationSource();
   addRunningElements();
-  fullpageFigures();
+  //fullpageFigures();
 };
 
 // exclude author photo
