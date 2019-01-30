ed6f4a30badea279bf2626a5f3a1e4bf7368f3c5
Matt Retzer <matthew.retzer@macmillan.com>
2019-01-30 16:00:43 -0500

    updating logo and other hardcoded styles in imprint specfic JS. For more deluxe designs: sidestepping Holt (for now), ignoring Picador, updating Flatiron

--- rsuite_assets/pdfmaker/scripts/swerve/pdf.js
+++ rsuite_assets/pdfmaker/scripts/swerve/pdf.js
@@ -15,7 +15,7 @@ function getAllElementsWithAttribute(tag, attribute, value)
 
 function moveIllustrationSource()
 {
-var illoSources = document.getElementsByClassName("IllustrationSourceis");
+var illoSources = document.querySelectorAll("figure p[class*='Credit-LineCrd']
 var copyright = getAllElementsWithAttribute('section',"data-type",'copyright-page')[0];
 for (var j = 0; illoSources.length > j; j++) {
 	var figID = illoSources[j].parentNode.getAttribute('id');
@@ -75,7 +75,7 @@ function fullpageFigures() {
     var textnode = document.createTextNode(" ");
     runFoot.setAttribute("class", "runfoot");
     runFoot.appendChild(textnode);
-    parentFig.setAttribute("class", "Illustrationholderill fullpage");
+    parentFig.setAttribute("class", "Image-PlacementImg fullpage");
     parentFig.insertBefore(runHeadLeft,parentFig.firstChild);
     parentFig.insertBefore(runHeadRight,parentFig.firstChild);
     parentFig.insertBefore(runFoot,parentFig.firstChild);
